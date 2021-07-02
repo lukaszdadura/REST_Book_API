2e8a1f819ed3e189d5d18221a035adb0b674c74d
lukaszdadura <83578617+lukaszdadura@users.noreply.github.com>
2021-07-02 10:08:45 +0200

    initial commit

---
+++ src/main/java/pl/lukaszdadura/bookapi/config/AppConfig.java
@@ -0,0 +1,15 @@
+package pl.lukaszdadura.bookapi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("pl.lukaszdadura.bookapi")
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+
+
+}
